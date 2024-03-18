99882e548cac1e186212aac888cdb0ce0d336348
Neilson Flemming <Neilson.Flemming@Shopify.com>
2024-03-18 11:41:50 -0400

    Added main to scripts

--- scripts/updateLocationAddress.py
+++ scripts/updateLocationAddress.py
@@ -6,48 +6,49 @@ from queries.locationQuery import locationQuery, locationPayload
 from mutations.locationEditMutation import locationEditQuery, locationEditInput, locationEditPayload
 from client.shopifyClient import shopifyClient
 
-# location query parameters
-locationId = None
-locationCount = 1
-locationHasNextPage = True
-locationEndCursor = "null"
+if __name__ == "__main__":
+  # location query parameters
+  locationId = None
+  locationCount = 1
+  locationHasNextPage = True
+  locationEndCursor = "null"
 
-client = shopifyClient()
+  client = shopifyClient()
 
-while locationHasNextPage == True:
-  # query for locations
-  locationQ = locationQuery(locationCount, locationEndCursor)
-  locationP = locationPayload(locationQ)
-  # print(locationQ)
+  while locationHasNextPage == True:
+    # query for locations
+    locationQ = locationQuery(locationCount, locationEndCursor)
+    locationP = locationPayload(locationQ)
+    # print(locationQ)
 
-  # make api call to get location data
-  response = client.request("POST", json=locationP)
-  #print(response.json())
-  locationR = response.json()
+    # make api call to get location data
+    response = client.request("POST", json=locationP)
+    #print(response.json())
+    locationR = response.json()
 
-  # update location query paramaters
-  locationId = locationR['data']['locations']['nodes'][0]['id']
-  locationHasNextPage = locationR['data']['locations']['pageInfo']['hasNextPage']
-  locationEndCursor = '"{}"'.format(locationR['data']['locations']['pageInfo']['endCursor'])
-  # print(locationId,locationHasNextPage,locationEndCursor)
+    # update location query paramaters
+    locationId = locationR['data']['locations']['nodes'][0]['id']
+    locationHasNextPage = locationR['data']['locations']['pageInfo']['hasNextPage']
+    locationEndCursor = '"{}"'.format(locationR['data']['locations']['pageInfo']['endCursor'])
+    # print(locationId,locationHasNextPage,locationEndCursor)
 
-  # generate real random address
-  address = real_random_address()
-  while not address.get('city'):
+    # generate real random address
     address = real_random_address()
-  print(address)
+    while not address.get('city'):
+      address = real_random_address()
+    print(address)
 
-  # create lcation update payloado
-  locationEditQ = locationEditQuery()
-  locationI = locationEditInput(locationId, address['address1'], address['address2'],
-                            address['city'], address['postalCode'])
+    # create lcation update payloado
+    locationEditQ = locationEditQuery()
+    locationI = locationEditInput(locationId, address['address1'], address['address2'],
+                              address['city'], address['postalCode'])
 
-  locationEditP = locationEditPayload(locationEditQ, locationI)
-  response = client.request("POST", json=locationEditP)
-  print(response.json())
+    locationEditP = locationEditPayload(locationEditQ, locationI)
+    response = client.request("POST", json=locationEditP)
+    print(response.json())
 
-  # print params
-  print(f'Update Params : {locationI})')
+    # print params
+    print(f'Update Params : {locationI})')
 
 
 
--- scripts/updateLocationInventory.py
+++ scripts/updateLocationInventory.py
@@ -8,69 +8,70 @@ from mutations.activateInventoryMutation import activateInventoryQuery, activate
 from queries.inventoryItemQuery import inventoryItemQuery, inventoryItemPayload
 from queries.locationQuery import locationQuery, locationPayload
 
-# location query parameters
-locationId = None
-locationCount = 1
-locationHasNextPage = True
-locationEndCursor = "null"
+if __name__ == "__main__":
+  # location query parameters
+  locationId = None
+  locationCount = 1
+  locationHasNextPage = True
+  locationEndCursor = "null"
 
-client = shopifyClient()
+  client = shopifyClient()
 
-while locationHasNextPage == True:
-  # query for locations
-  locationQ = locationQuery(locationCount, locationEndCursor)
-  locationP = locationPayload(locationQ)
-  # print(locationQ)
+  while locationHasNextPage == True:
+    # query for locations
+    locationQ = locationQuery(locationCount, locationEndCursor)
+    locationP = locationPayload(locationQ)
+    # print(locationQ)
 
-  # make api call to get location data
-  response = client.request("POST", json=locationP)
-  # print(response.json())
-  locationR = response.json()
-
-  # update location query paramaters
-  locationId = locationR['data']['locations']['nodes'][0]['id']
-  locationHasNextPage = locationR['data']['locations']['pageInfo']['hasNextPage']
-  locationEndCursor = '"{}"'.format(locationR['data']['locations']['pageInfo']['endCursor'])
-  # print(locationId,locationHasNextPage,locationEndCursor)
-
-  # inventory item query parameters
-  inventoryItemId = None
-  inventoryItemCount = 1
-  inventoryItemHasNextPage = True
-  inventoryItemEndCursor = "null"
-
-  while inventoryItemHasNextPage:
-    # query for inventory item
-    inventoryItemQ = inventoryItemQuery(inventoryItemCount, inventoryItemEndCursor)
-    inventoryItemP = inventoryItemPayload(inventoryItemQ)
-    # print(inventoryItemQ)
-
-    # make api call to get inventory item data
-    response = client.request("POST", json=inventoryItemP)
+    # make api call to get location data
+    response = client.request("POST", json=locationP)
     # print(response.json())
-    inventoryItemR = response.json()
+    locationR = response.json()
 
-    # update inventory item query paramaters
-    inventoryItemId = inventoryItemR['data']['inventoryItems']['edges'][0]['node']['id']
-    inventoryItemHasNextPage = inventoryItemR['data']['inventoryItems']['pageInfo']['hasNextPage']
-    inventoryItemEndCursor = '"{}"'.format(inventoryItemR['data']['inventoryItems']['pageInfo']['endCursor'])
-    # print(inventoryItemId, inventoryItemNextPage, inventoryItemEndCursor)
+    # update location query paramaters
+    locationId = locationR['data']['locations']['nodes'][0]['id']
+    locationHasNextPage = locationR['data']['locations']['pageInfo']['hasNextPage']
+    locationEndCursor = '"{}"'.format(locationR['data']['locations']['pageInfo']['endCursor'])
+    # print(locationId,locationHasNextPage,locationEndCursor)
 
-    # mutation using stored location and inventory
-    activateInventoryQ = activateInventoryQuery()
-    activateInventoryI = activateInventoryInput(inventoryItemId, locationId, random.randint(0, 25))
-    activateInventoryP = activateInventoryPayload(activateInventoryQ, activateInventoryI)
+    # inventory item query parameters
+    inventoryItemId = None
+    inventoryItemCount = 1
+    inventoryItemHasNextPage = True
+    inventoryItemEndCursor = "null"
 
-    # make api call to activate and set inventory
-    response = client.request("POST", json=activateInventoryP)
-    print(f'Status Code: ', response.status_code)
+    while inventoryItemHasNextPage:
+      # query for inventory item
+      inventoryItemQ = inventoryItemQuery(inventoryItemCount, inventoryItemEndCursor)
+      inventoryItemP = inventoryItemPayload(inventoryItemQ)
+      # print(inventoryItemQ)
 
-    activateInventoryR = response.json()
-    available = activateInventoryR['data']['inventoryActivate']['inventoryLevel']['available']
-    apiLimit = activateInventoryR['extensions']['cost']['throttleStatus']['currentlyAvailable']
+      # make api call to get inventory item data
+      response = client.request("POST", json=inventoryItemP)
+      # print(response.json())
+      inventoryItemR = response.json()
 
-    # print params
-    print(f'Location ID : {locationId})')
-    print(f'Item ID : {inventoryItemId}')
-    print(f'Available : {available}')
-    print(f'API Limit : {apiLimit}')
+      # update inventory item query paramaters
+      inventoryItemId = inventoryItemR['data']['inventoryItems']['edges'][0]['node']['id']
+      inventoryItemHasNextPage = inventoryItemR['data']['inventoryItems']['pageInfo']['hasNextPage']
+      inventoryItemEndCursor = '"{}"'.format(inventoryItemR['data']['inventoryItems']['pageInfo']['endCursor'])
+      # print(inventoryItemId, inventoryItemNextPage, inventoryItemEndCursor)
+
+      # mutation using stored location and inventory
+      activateInventoryQ = activateInventoryQuery()
+      activateInventoryI = activateInventoryInput(inventoryItemId, locationId, random.randint(0, 25))
+      activateInventoryP = activateInventoryPayload(activateInventoryQ, activateInventoryI)
+
+      # make api call to activate and set inventory
+      response = client.request("POST", json=activateInventoryP)
+      print(f'Status Code: ', response.status_code)
+
+      activateInventoryR = response.json()
+      available = activateInventoryR['data']['inventoryActivate']['inventoryLevel']['available']
+      apiLimit = activateInventoryR['extensions']['cost']['throttleStatus']['currentlyAvailable']
+
+      # print params
+      print(f'Location ID : {locationId})')
+      print(f'Item ID : {inventoryItemId}')
+      print(f'Available : {available}')
+      print(f'API Limit : {apiLimit}')
